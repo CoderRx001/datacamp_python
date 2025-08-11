51cf4157b773ec178a60a18089d3de1e8a170428
CoderRx001 <carrillo964@gmail.com>
2025-08-10 23:44:16 -0500

    level 6.1 update

--- intro_06_1.py
+++ intro_06_1.py
@@ -2,7 +2,7 @@ savings = 100
 new_savings = 40
 
 # Calculate total_savings using savings and new_savings
-____
+total_savings= savings + new_savings
 print(total_savings)
 
 # Print the type of total_savings
